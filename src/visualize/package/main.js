f79c6d35d439a96c392552511b7934b5bb950b1f
Sharadh Krishnamurthy <sharadhk.murthy@gmail.com>
2016-10-14 22:00:59 -0400

    Directed edges from/to edge of nodes.

--- src/visualize/package/main.js
+++ src/visualize/package/main.js
@@ -1,5 +1,6 @@
 /**
- * Reference
+ * References
+ * http://alignedleft.com/tutorials/d3/binding-data
  * http://jsdatav.is/visuals.html?id=83515b77c2764837aac2
  * http://bl.ocks.org/mbostock/1153292
  *
@@ -54,42 +55,6 @@
       {
         "source": 0, 
         "target": 5
-      }, 
-      {
-        "source": 1, 
-        "target": 2
-      }, 
-      {
-        "source": 1, 
-        "target": 3
-      }, 
-      {
-        "source": 3, 
-        "target": 2
-      }, 
-      {
-        "source": 4, 
-        "target": 0
-      }, 
-      {
-        "source": 4, 
-        "target": 1
-      }, 
-      {
-        "source": 4, 
-        "target": 2
-      }, 
-      {
-        "source": 4, 
-        "target": 3
-      }, 
-      {
-        "source": 4, 
-        "target": 5
-      }, 
-      {
-        "source": 5, 
-        "target": 2
       }
     ]
   }
@@ -101,14 +66,26 @@
     .attr('height', height);
 
   var animationStep = 400;
+  var nodeRadius = width/25;
   
   var force = null,
     nodes = null,
     links = null,
     names = null;
 
-  function initForce() {
+  function init() {
     svg.selectAll('*').remove();
+
+    svg.append('defs').append('marker')
+      .attr('id', 'program')
+      .attr('viewBox', '0 0 10 10')
+      .attr('refX', '10')
+      .attr('refY', '5')
+      .attr('orient', 'auto')
+      .attr('markerWidth', '5')
+      .attr('markerHeight', '5')
+      .append('path')
+        .attr('d', 'M0,0L10,5L0,10');
 
     force = d3.layout.force()
       .size([width, height])
@@ -120,74 +97,72 @@
     nodes = svg.append('g').selectAll('.node')
       .data(graph.nodes)
       .enter().append('circle')
-      .attr('class', 'node')
-      .attr('r', width/25);
+        .attr('class', 'node')
+        .attr('r', function(d) {
+          d.radius = nodeRadius;
+          return d.radius;
+        });
     
     links = svg.append('g').selectAll('.link')
       .data(graph.links)
-      .enter().append('line')
-      .attr('class', 'link');
+      .enter().append('path')
+        .attr('class', 'link')
+        .attr('marker-end', 'url(#program)');
 
     names = svg.append('g').selectAll("text")
       .data(graph.nodes)
       .enter().append("text")
-      .attr("x", 8)
-      .attr("y", ".31em")
-      .text(function(d) {
-        return d.name;
-      });
+        .attr("x", 8)
+        .attr("y", ".31em")
+        .text(function(d) {
+          return d.name;
+        });
 
-    force.on('tick', stepForce);
+    force.on('tick', step);
   }
 
-  function stepForce() {
-    var updatedNodes,
-      updatedLinks;
+  function linkArc(d) {
+    var dx = d.target.x - d.source.x,
+      dy = d.target.y - d.source.y,
+      dr = Math.sqrt(dx * dx + dy * dy);
+    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+  }
 
-    if (force.fullSpeed) {
-      updatedNodes = nodes;
-      updatedLinks = links;
-      updatedNames = names;
-    } else {
-      updatedNodes = nodes.transition()
-        .ease('linear')
-        .duration(animationStep);
-      updatedLinks = links.transition()
-        .ease('linear')
-        .duration(animationStep);
-      updatedNames = names.transition()
-        .ease('linear')
-        .duration(animationStep);
-    }
+  function linkLine(d) {
+    var dx = d.target.x - d.source.x,
+      dy = d.target.y - d.source.y;
 
-    updatedNodes
+    var dMag = Math.sqrt(dx * dx + dy * dy);
+
+    // Vector addition: dx/dMag and dy/dMag are unit vectors
+    var offsetX = d.target.radius * dx / dMag;
+    var offsetY = d.target.radius * dy / dMag;
+    
+    // Apply appropriate direction offset 
+    // so link starts/ends beyond node circle...
+    var targetX = d.target.x - offsetX;
+    var targetY = d.target.y - offsetY;
+
+    var sourceX = d.source.x + offsetX;
+    var sourceY = d.source.y + offsetY;
+    
+    return "M" + sourceX + "," + sourceY +
+      "L" + targetX + "," + targetY;
+  }
+
+  function step() {
+    nodes
       .attr('cx', function(d) { return d.x; })
       .attr('cy', function(d) { return d.y; });
 
-    updatedNames
+    names
       .attr('x', function(d) { return d.x; })
       .attr('y', function(d) { return d.y; });
 
-    updatedLinks
-      .attr('x1', function(d) { return d.source.x; })
-      .attr('y1', function(d) { return d.source.y; })
-      .attr('x2', function(d) { return d.target.x; })
-      .attr('y2', function(d) { return d.target.y; });
-
-    if (!force.fullSpeed) {
-      force.stop();
-
-      if (force.slowMotion) {
-        setTimeout(
-          function () { force.start(); },
-          animationStep
-        );
-      }
-    }
+    links.attr('d', linkLine);
   }
 
-  initForce();
-  force.fullSpeed = true;
+  init();
   force.start();
 
 }())
